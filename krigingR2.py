beautify plots, fix ln(det(x)) calculation for many inputs

krigingR2.py
@@ -6,6 +6,7 @@ from matplotlib import rc
 import matplotlib
 import scipy
 from scipy.optimize import minimize
+from matplotlib.colors import LogNorm
 
 FONT_SIZE=14
 font = {'family':'sans-serif', 'size':FONT_SIZE}
@@ -28,11 +29,11 @@ def calc_corMat(knwon_x, theta, p, plot_it=False):
     expFunc = []
     for row in range(0, n):
         for column in range(0, n):
-            corMat[row][column] = math.exp(-theta * abs(knwon_x[row] - knwon_x[column]) ** p)
+            corMat[row][column] = math.exp(-theta * (abs(knwon_x[row] - knwon_x[column]) ** p))
             # since this is a symetric mat...
             # corMat[column][row] = corMat[row][column]
             diff.append(knwon_x[row] - knwon_x[column])
-            expFunc.append(math.exp(-theta * abs(knwon_x[row] - knwon_x[column]) ** p))
+            expFunc.append(math.exp(-theta * (abs(knwon_x[row] - knwon_x[column]) ** p)))
 
     if plot_it:
         fig, ax = plt.subplots()
@@ -54,15 +55,18 @@ def calc_likelihood(known_x, known_val, theta, p):
     corMat = calc_corMat(known_x, theta, p)
     n = len(known_val)
     known_val = np.array(known_val)
-    LnDetCorMat = np.log(np.linalg.det(corMat))
+    #LnDetCorMat = np.log(np.linalg.det(corMat))
+    LnDetCorMat = np.linalg.slogdet(corMat)[1]
     one = np.ones((n, 1)).flatten()
     mu = (np.transpose(one) @ np.linalg.inv(corMat) @ known_val) / (np.transpose(one) @ np.linalg.inv(corMat) @ one)
     SigmaSqr = (np.transpose(known_val - one * mu) @ np.linalg.inv(corMat) @ (known_val - one * mu)) / n
     NegLnLike = (-1) * (-(n / 2) * np.log(SigmaSqr) - 0.5 * LnDetCorMat)
+    if np.isnan(NegLnLike):
+        print('Error: nan')
     return NegLnLike
 
 def calc_likelihood_opti(params, *args):
-    return calc_likelihood(args[0], args[1], params[0], args[2])
+    return calc_likelihood(args[0], args[1], params[0], params[1])
 
 def predict(x_pred, knwon_x, knwon_val, theta, p):
     n = len(knwon_val)
@@ -71,7 +75,7 @@ def predict(x_pred, knwon_x, knwon_val, theta, p):
     mu = (np.transpose(one) @ np.linalg.inv(corMat) @ knwon_val) / (np.transpose(one) @ np.linalg.inv(corMat) @ one)
     psi = np.ones((n, 1)).flatten()
     for i in range(0, len(psi)):
-        psi[i] = math.exp(-theta * abs(knwon_x[i] - x_pred) ** p)
+        psi[i] = math.exp(-theta * (abs(knwon_x[i] - x_pred) ** p))
     fx = mu + np.transpose(psi) @ np.linalg.inv(corMat) @ (knwon_val - one * mu)
     return fx
 
@@ -93,32 +97,58 @@ if __name__ == '__main__':
     NegLnLike = calc_likelihood(px, py, theta, p)
     print('negLnLike = ' + str(NegLnLike))
 
-    thetas = np.linspace(0.01, 10, 5000+1)
-    likely = []
-    for thet in thetas:
-        likely.append(calc_likelihood(px, py, thet, p))
+    #thetas = np.linspace(0.01, 10, 1000+1)
+    thetas = np.logspace(-2, 1, num=5000)
+    ps = [2.]#np.linspace(1., 2., 100)
+    likely = np.zeros((len(ps), len(thetas)))
+    for it in range(0, len(thetas)):
+        for ip in range(0, len(ps)):
+            likely[ip][it] = calc_likelihood(px, py, thetas[it], ps[ip])
 
-    x0 = 1.
-    bnds = [(0.01, 1000.)]
-    res = minimize(calc_likelihood_opti, x0, args=(px, py, p), method='SLSQP', tol=1e-11, bounds=bnds)
+    x0 = [1., 2.]
+    bnds = [(0.0001, 1000.), (1., 2.)]
+    res = minimize(calc_likelihood_opti, x0, args=(px, py), method='SLSQP', tol=1e-11, bounds=bnds)
 
     bestTheta = res.x[0]
-    minLike = calc_likelihood(px, py, bestTheta, p)
+    bestP = res.x[1]
+    minLike = calc_likelihood(px, py, bestTheta, bestP)
     print('minLike = '+str(minLike))
     print('@theta = ' + str(bestTheta))
-    plt.semilogx(thetas, likely)
-    plt.semilogx(bestTheta, minLike, 'rx')
+    print('@p = ' + str(bestP))
+    fig, ax = plt.subplots()
+    rc('text', usetex=True)
+    rc('font', **font)
+    rc('xtick', labelsize=FONT_SIZE)
+    rc('ytick', labelsize=FONT_SIZE)
+    ax.semilogx(thetas, likely[-1])
+    ax.semilogx(bestTheta, minLike, 'rx', label='Minimum')
+    ax.set_xlabel(r'$\theta$', fontdict=font)
+    ax.set_ylabel(r'Likelihood', fontdict=font)
+    ax.legend(loc='upper right', ncol=1)
+    fig.set_size_inches(7, 4)
+    plt.tight_layout()
+    plt.savefig('dataOut/krigingR2likelihood.svg')
+    plt.savefig('dataOut/krigingR2likelihood.pdf')
     plt.show()
 
     fig, ax = plt.subplots()
-    rc('text', usetex=True)
+    ax.set_xscale('log')
+    pcol = ax.pcolor(thetas, ps, likely, cmap='viridis_r')
+    fig.colorbar(pcol)
+    ax.plot(bestTheta, bestP, 'rx')
+    ax.set_xlabel('$\theta$', fontdict=font)
+    ax.set_ylabel('p', fontdict=font)
+    plt.show()
+
+    fig, ax = plt.subplots()
+    #rc('text', usetex=True)
     rc('font', **font)
     rc('xtick', labelsize=FONT_SIZE)
     rc('ytick', labelsize=FONT_SIZE)
 
     krigY = np.zeros((len(fx),1)).flatten()
     for i in range(0, len(fx)):
-        krigY[i] = predict(fx[i], px, py, bestTheta, p)
+        krigY[i] = predict(fx[i], px, py, bestTheta, bestP)
     ax.plot(fx, krigY, 'b-', label=r'$f_{kriging}$ mit $\theta = '+'{0:.3f}'.format(bestTheta)+'$')
 
     ax.plot(fx, fy, 'r-', label=r'$f_{original}$')

krigingRn.py
@@ -62,15 +62,20 @@ def calc_likelihood(known_x, known_val, theta, p):
     k = known_x.shape[0]
     n = known_x.shape[1]
     known_val = np.array(known_val)
-    LnDetCorMat = np.log(np.linalg.det(corMat))
+    #LnDetCorMat = np.log(np.linalg.det(corMat))
+    LnDetCorMat = np.linalg.slogdet(corMat)[1]
     one = np.ones((n, 1)).flatten()
     mu = (np.transpose(one) @ np.linalg.inv(corMat) @ known_val) / (np.transpose(one) @ np.linalg.inv(corMat) @ one)
     SigmaSqr = (np.transpose(known_val - one * mu) @ np.linalg.inv(corMat) @ (known_val - one * mu)) / n
     NegLnLike = (-1) * (-(n / 2) * np.log(SigmaSqr) - 0.5 * LnDetCorMat)
+    if NegLnLike == float('nan'):
+        print('Error: nan')
     return NegLnLike
 
 def calc_likelihood_opti(params, *args):
-    return calc_likelihood(args[0], args[1], params, args[2])
+    NegLnLike = calc_likelihood(args[0], args[1], params, args[2])
+    print(str(NegLnLike))
+    return NegLnLike
 
 def predict(x_pred, known_x, knwon_val, theta, p):
     k = known_x.shape[0]
@@ -117,16 +122,24 @@ if __name__ == '__main__':
 
     p = [2., 2.]
 
-    thetas = np.linspace(0.01, 10, 100 + 1)
+    #thetas = np.linspace(0.001, 0.1, 100 + 1)
+    thetas = np.logspace(-3, 1, num=20)
     likelyX1 = []
     likelyX2 = []
-    for thet in thetas:
-        likelyX1.append(calc_likelihood(knownParams, knownValues, [thet, 1.], p))
-        likelyX2.append(calc_likelihood(knownParams, knownValues, [1., thet], p))
+    likely = np.zeros((len(thetas), len(thetas)))
+    for i1 in range(0, len(thetas)):
+        print(str(i1) + ' / ' + str(len(thetas)))
+        for i2 in range(0, len(thetas)):
+            #likelyX1.append(calc_likelihood(knownParams, knownValues, [thetas[i1], thetas[i2]], p))
+            #likelyX2.append(calc_likelihood(knownParams, knownValues, [1., thet], p))
+            likely[i1][i2] = calc_likelihood(knownParams, knownValues, [thetas[i1], thetas[i2]], p)
 
     x0 = [1., 1.]
-    bnds = [(0.01, 1000.), (0.01, 1000.)]
-    res = minimize(calc_likelihood_opti, x0, args=(knownParams, knownValues, p), method='SLSQP', tol=1e-11, bounds=bnds)
+    bnds = [(0.0001, 1000.), (0.0001, 1000.)]
+    opt = {}
+    opt['disp'] = True
+    opt['maxiter'] = 99999
+    res = minimize(calc_likelihood_opti, x0, args=(knownParams, knownValues, p), method='SLSQP', tol=1e-6, options=opt, bounds=bnds)
 
     bestThetaX1 = res.x[0]
     minLikeX1 = calc_likelihood(knownParams, knownValues, [bestThetaX1, 1.], p)
@@ -134,13 +147,22 @@ if __name__ == '__main__':
     minLikeX2 = calc_likelihood(knownParams, knownValues, [1., bestThetaX2], p)
     bestTheta = [res.x[0], res.x[1]]
     minLike = calc_likelihood(knownParams, knownValues, [bestThetaX1, bestThetaX2], p)
-    #print('minLike = ' + str(minLike))
-    #print('@theta = ' + str(bestTheta))
-    plt.semilogx(thetas, likelyX1, '-b')
-    plt.semilogx(thetas, likelyX2, '-g')
-    plt.semilogx(bestThetaX1, minLikeX1, 'bx')
-    plt.semilogx(bestThetaX2, minLikeX2, 'gx')
-    plt.semilogx([0, 1], [minLike, minLike], 'rx')
+    print('minLike = ' + str(minLike))
+    print('@theta1 = ' + str(bestThetaX1))
+    print('@theta2 = ' + str(bestThetaX2))
+    fig, ax = plt.subplots()
+    ax.set_yscale('log')
+    ax.set_xscale('log')
+    pcol = ax.pcolor(thetas, thetas, likely)
+    fig.colorbar(pcol)
+    ax.plot(bestThetaX1, bestThetaX2, 'rx')
+    #ax.plot([bestThetaX1], [bestThetaX2], 'rx')
+
+    #plt.semilogx(thetas, likelyX1, '-b')
+    #plt.semilogx(thetas, likelyX2, '-g')
+    #plt.semilogx(bestThetaX1, minLikeX1, 'bx')
+    #plt.semilogx(bestThetaX2, minLikeX2, 'gx')
+    #plt.semilogx([0, 1], [minLike, minLike], 'rx')
     plt.show()
 
 