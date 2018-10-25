6d8c6e8ccafd55dd8bf5e9c1c9013092a82d1b6d
Juri Bieler <juribieler@gmail.com>
2018-10-25 16:51:00 +0200

    use old sample points but mae for polynom fitting

--- wingconstruction/surrogate_v3.py
+++ wingconstruction/surrogate_v3.py
@@ -124,7 +124,7 @@ class Surrogate:
         for i in range(0, len(orders)):
             self.train_model(SURRO_POLYNOM, [orders[i]])
             self.run_validation(full_validation=False)
-            rmse[i] = self.results.vali_results.rmse
+            rmse[i] = self.results.vali_results.mae
         best_order = orders[np.argmin(rmse)]
         self.results.opti_params = [best_order]
         self.train_model(SURRO_POLYNOM, [best_order])
@@ -306,8 +306,8 @@ class Surrogate:
         self.vali_params = np.array([[center_r, center_s],
                                 [center_r - int(round(d_r / 4.) * factor), center_s - (factor * d_s / 4.)],
                                 [center_r - int(round(d_r / 3.) * factor), center_s + (factor * d_s / 3.)],
-                                [center_r + int(round(d_r / 2.7) * factor), center_s + (factor * d_s / 2.5)],
-                                [center_r + int(round(d_r / 2.2) * factor), center_s - (factor * d_s / 1.2)]])
+                                [center_r + int(round(d_r / 2.7) * factor), center_s + (factor * d_s / 2.7)],
+                                [center_r + int(round(d_r / 2.2) * factor), center_s - (factor * d_s / 2.2)]])
         self.vali_params_s = np.zeros(self.vali_params.shape)
         self.vali_params_s[:, 0] = (self.vali_params[:, 0] - self.offset_rib) / self.scale_rib
         self.vali_params_s[:, 1] = (self.vali_params[:, 1] - self.offset_shell) / self.scale_shell
@@ -333,6 +333,8 @@ class Surrogate:
         else:
             rmse = vali.calc_rmse(self.vali_params_s, self.vali_values, self.surro.predict)
             self.results.vali_results.rmse = rmse
+            mae = vali.calc_mae(self.vali_params_s, self.vali_values, self.surro.predict)
+            self.results.vali_results.mae = mae
         if self.show_plots and full_validation:
             deri_plot = PlotHelper(['Rippen', 'Blechdicke in mm'], fancy=FANCY_PLOT, pgf=self.pgf)
             dev = np.zeros(self.stress.shape)
@@ -531,7 +533,7 @@ if __name__ == '__main__':
     # SURRO_KRIGING, SURRO_RBF, SURRO_POLYNOM, SURRO_PYKRIGING, SURRO_RBF_SCIPY
     if True:
         sur = Surrogate(use_abaqus=True, pgf=PGF, show_plots=SHOW_PLOT, scale_it=True)
-        res, _ = sur.auto_run(SAMPLE_HALTON, 17, SURRO_POLYNOM, run_validation=True, auto_fit=True, sequential_runs=0) # 'gaus' 'multi-quadratic'
+        res, _ = sur.auto_run(SAMPLE_HALTON, 17, SURRO_POLYNOM, run_validation=False, auto_fit=True, sequential_runs=0) # 'gaus' 'multi-quadratic'
     else:
         SHOW_PLOT = False
         SAMPLING = SAMPLE_HALTON
